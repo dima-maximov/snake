f50cca281cf7f5a7386da5b09c80d3ae9e957884
Dima Maximov <dima.max.1994@gmail.com>
2014-09-19 20:34:01 +0400

    Был изменен код тела змеи и яблока.

--- main.cpp
+++ main.cpp
@@ -35,8 +35,10 @@ void drawField(IplImage *field, int cell, int box_x, int box_y)
 
 }
 
-void drawFood(IplImage *img,int cell, int box_x , int box_y,CvPoint &appel, int r,int g,int b, int &ran)
+void drawFood(IplImage *img,int cell, int box_x , int box_y,CvPoint &appel, int r,int g,int b, int &ran, vector<CvPoint> &body, int number_of_bodies, int &number_of_apples)
 {
+    number_of_bodies = 0;
+
     for (int i = 0; i < ran; i++)
     {
         r = (rand() % 200) + 25;
@@ -47,120 +49,41 @@ void drawFood(IplImage *img,int cell, int box_x , int box_y,CvPoint &appel, int 
         appel.x = appel_x * cell+cell/2;
         appel.y = appel_y * cell+cell/2;
     }
+    //если яблоко появится на змее, то яблоко поменяет свои коордианты
+    for(int i = 0;i < number_of_apples ; i++)
+    {
+        if (body.at(number_of_bodies).x ==  appel.x && body.at(number_of_bodies).y ==  appel.y)
+        {
+            int appel_x = rand() % box_x;
+            int appel_y = rand() % box_y;
+            appel.x = appel_x * cell+cell/2;
+            appel.y = appel_y * cell+cell/2;
+        }
+        number_of_bodies++;
+    }
 
     cvCircle(img, appel, 10, CV_RGB(r, g, b), -1, 8);
 }
 
-void drawSnake(vector<CvPoint> &body, IplImage *currFrame, int &number_of_apples)
+void drawSnake(vector<CvPoint> &body, IplImage *currFrame, int &number_of_apples, int number_of_bodies, int end, int finish)
 {
+    //begin - место на которое перемещается тело end(кот. стоит за ним)
+    int begin;
     //перемещение тела на место тела кот. было перед ним
-    body.at(23) = body.at(22);
-    body.at(22) = body.at(21);
-    body.at(21) = body.at(20);
-    body.at(20) = body.at(19);
-    body.at(19) = body.at(18);
-    body.at(18) = body.at(17);
-    body.at(17) = body.at(16);
-    body.at(16) = body.at(15);
-    body.at(15) = body.at(14);
-    body.at(14) = body.at(13);
-    body.at(13) = body.at(12);
-    body.at(12) = body.at(11);
-    body.at(11) = body.at(10);
-    body.at(10) = body.at(9);
-    body.at(9) = body.at(8);
-    body.at(8) = body.at(7);
-    body.at(7) = body.at(6);
-    body.at(6) = body.at(5);
-    body.at(5) = body.at(4);
-    body.at(4) = body.at(3);
-    body.at(3) = body.at(2);
-    body.at(2) = body.at(1);
-    body.at(1) = body.at(0);
-
+    for(int i = 0; i <  finish + 3; i++)
+    {
+        begin = end - 1;
+        body.at(end) = body.at(begin);
+        end--;
+    }
     cvCircle( currFrame, body.at(1), 20, CV_RGB( 250, 250, 0), -1, 8);
     cvCircle( currFrame, body.at(2), 20, CV_RGB( 250, 250, 0), -1, 8);
 
     // создаем тело после съеденного яблока
-    if (number_of_apples > 0)
+    for(int i = 0; i < number_of_apples ; i++)
     {
-        cvCircle( currFrame, body.at( 3 ), 20, CV_RGB( 10, 110, 110), -1, 8);
-    }
-    if (number_of_apples > 1)
-    {
-        cvCircle( currFrame, body.at( 4 ), 20, CV_RGB( 10, 210, 210), -1, 8);
-    }
-    if (number_of_apples > 2)
-    {
-        cvCircle( currFrame, body.at( 5 ), 20, CV_RGB( 210, 10, 110), -1, 8);
-    }
-    if (number_of_apples > 3)
-    {
-        cvCircle( currFrame, body.at( 6 ), 20, CV_RGB( 150, 10, 110), -1, 8);
-    }
-    if (number_of_apples > 4)
-    {
-        cvCircle( currFrame, body.at( 7 ), 20, CV_RGB( 210, 20, 10), -1, 8);
-    }
-    if (number_of_apples > 5)
-    {
-        cvCircle( currFrame, body.at( 8 ), 20, CV_RGB( 210, 10, 210), -1, 8);
-    }
-    if (number_of_apples > 6)
-    {
-        cvCircle( currFrame, body.at( 9 ), 20, CV_RGB( 10, 210, 110), -1, 8);
-    }
-    if (number_of_apples > 7)
-    {
-        cvCircle( currFrame, body.at( 10 ), 20, CV_RGB( 160, 150, 110), -1, 8);
-    }
-    if (number_of_apples > 8)
-    {
-        cvCircle( currFrame, body.at( 11 ), 20, CV_RGB( 70, 160, 210), -1, 8);
-    }
-    if (number_of_apples > 9)
-    {
-        cvCircle( currFrame, body.at( 12 ), 20, CV_RGB( 100, 10, 210), -1, 8);
-    }
-    if (number_of_apples > 10)
-    {
-        cvCircle( currFrame, body.at( 13 ), 20, CV_RGB( 0, 210, 210), -1, 8);
-    }
-    if (number_of_apples > 11)
-    {
-        cvCircle( currFrame, body.at( 14 ), 20, CV_RGB( 210, 210, 20), -1, 8);
-    }
-    if (number_of_apples > 12)
-    {
-        cvCircle( currFrame, body.at( 15 ), 20, CV_RGB( 70, 80, 110), -1, 8);
-    }
-    if (number_of_apples > 13)
-    {
-        cvCircle( currFrame, body.at( 16 ), 20, CV_RGB( 150, 10, 110), -1, 8);
-    }
-    if (number_of_apples > 14)
-    {
-        cvCircle( currFrame, body.at( 17 ), 20, CV_RGB( 210, 20, 210), -1, 8);
-    }
-    if (number_of_apples > 15)
-    {
-        cvCircle( currFrame, body.at( 18 ), 20, CV_RGB( 250, 110, 10), -1, 8);
-    }
-    if (number_of_apples > 16)
-    {
-        cvCircle( currFrame, body.at( 19 ), 20, CV_RGB( 10, 200, 240), -1, 8);
-    }
-    if (number_of_apples > 17)
-    {
-        cvCircle( currFrame, body.at( 20 ), 20, CV_RGB( 100, 250, 200), -1, 8);
-    }
-    if (number_of_apples > 18)
-    {
-        cvCircle( currFrame, body.at( 21 ), 20, CV_RGB( 0, 250, 170), -1, 8);
-    }
-    if (number_of_apples > 19)
-    {
-        cvCircle( currFrame, body.at( 22 ), 20, CV_RGB( 90, 0, 120), -1, 8);
+        cvCircle( currFrame, body.at(number_of_bodies), 20, CV_RGB( 255, 255, 0), -1, 8);
+        number_of_bodies++;
     }
 
 }
@@ -187,7 +110,7 @@ void moveSnake( char &dir, vector<CvPoint> &body, int cell)
         body.at(0).y = body.at(0).y - cell;
 
 }
-void end_snake(char &direc, int &finish, int &number_of_apples, vector<CvPoint> &body, int &width_field, int &height_field, int &exit)
+void end_snake(char &direc, int &finish, int &number_of_apples, vector<CvPoint> &body, int &width_field, int &height_field, int &exit, int number_of_bodies)
 {
     // условия выхода из цикла
     if (direc == 27 || finish == number_of_apples || body.at(0).x < 0 || body.at(0).x  == width_field || body.at(0).x > width_field || body.at(0).y < 0 || body.at(0).y == height_field || body.at(0).y > height_field)
@@ -200,29 +123,13 @@ void end_snake(char &direc, int &finish, int &number_of_apples, vector<CvPoint> 
         exit=1;
     }
     //выходим если созданное  тело после съеденного яблока пересечется с головой
-    if ((( number_of_apples > 1 )&&( body.at ( 4 ).x == body.at(0).x  && body.at ( 4).y == body.at(0).y )) ||
-            ((number_of_apples > 2)&&(body.at ( 5 ).x == body.at(0).x  && body.at ( 5 ).y == body.at(0).y))||
-            ((number_of_apples > 3)&&(body.at ( 6 ).x == body.at(0).x  && body.at ( 6 ).y == body.at(0).y))||
-            ((number_of_apples > 4)&&(body.at ( 7 ).x == body.at(0).x  && body.at ( 7 ).y == body.at(0).y))||
-            ((number_of_apples > 5)&&(body.at ( 8 ).x == body.at(0).x  && body.at ( 8 ).y == body.at(0).y))||
-            ((number_of_apples > 6)&&(body.at ( 9 ).x == body.at(0).x  && body.at ( 9 ).y == body.at(0).y))||
-            ((number_of_apples > 7)&&(body.at ( 10 ).x == body.at(0).x  && body.at ( 10 ).y == body.at(0).y))||
-            ((number_of_apples > 8)&&(body.at ( 11 ).x == body.at(0).x  && body.at ( 11 ).y == body.at(0).y))||
-            ((number_of_apples > 9)&&(body.at ( 12).x == body.at(0).x  && body.at ( 12 ).y == body.at(0).y))||
-            ((number_of_apples > 10)&&(body.at ( 13 ).x == body.at(0).x  && body.at ( 13 ).y == body.at(0).y))||
-            ((number_of_apples > 11)&&(body.at ( 14 ).x == body.at(0).x  && body.at ( 14 ).y == body.at(0).y))||
-            ((number_of_apples > 12)&&(body.at ( 15 ).x == body.at(0).x  && body.at ( 15 ).y == body.at(0).y))||
-            ((number_of_apples > 13)&&(body.at ( 16 ).x == body.at(0).x  && body.at ( 16 ).y == body.at(0).y))||
-            ((number_of_apples > 14)&&(body.at ( 17 ).x == body.at(0).x  && body.at ( 17 ).y == body.at(0).y))||
-            ((number_of_apples > 15)&&(body.at ( 18 ).x == body.at(0).x  && body.at ( 18 ).y == body.at(0).y))||
-            ((number_of_apples > 16)&&(body.at ( 19 ).x == body.at(0).x  && body.at ( 19).y == body.at(0).y))||
-            ((number_of_apples > 17)&&(body.at ( 20 ).x == body.at(0).x  && body.at ( 20 ).y == body.at(0).y))||
-            ((number_of_apples > 18)&&(body.at ( 21).x == body.at(0).x  && body.at ( 21 ).y == body.at(0).y))||
-            ((number_of_apples > 19)&&(body.at ( 22 ).x == body.at(0).x  && body.at ( 22 ).y == body.at(0).y))||
-            ((number_of_apples > 0)&&(body.at ( 3 ).x == body.at(0).x  && body.at ( 3 ).y == body.at(0).y)))
-
+    for(int i = 0;i < number_of_apples ; i++)
     {
-        exit=1;
+        if (body.at ( number_of_bodies ).x == body.at(0).x  && body.at ( number_of_bodies ).y == body.at(0).y)
+        {
+            exit=1;
+        }
+        number_of_bodies++;
     }
 }
 int main()
@@ -234,7 +141,7 @@ int main()
     vector<CvPoint> body;           // для хранения элементов тела змеи
     CvPoint head;
 
-    int finish = 20;
+    int finish = 25;
     char direction = 83;
     int width_field = 500;
     int height_field = 500;
@@ -243,10 +150,10 @@ int main()
     int box_y = height_field/cell;
     int exit = 0;
 
-    cout << "количество квадратов: "    << box_x*box_y <<endl;
+    cout << "количество квадратов: "    << box_x * box_y << endl;
     cout << "количество в 1-м столбе: " << box_y << endl;
     cout << "количество в 1-й строке: " << box_x << endl;
-    cout << "ширина и высота квадрата : " << cell <<endl;
+    cout << "ширина и высота квадрата : " << cell << endl;
 
     field = cvCreateImage( cvSize ( width_field, height_field) , 8, 3);
     cvSet( field, cvScalar( 255, 255, 255));
@@ -286,6 +193,10 @@ int main()
 
     //создаем рандом
     int ran=1;
+    //номер тела после первого съеденного яблока
+    int number_of_bodies = 3;
+    //номер последнего тела(хвоста)
+    int end = finish + 3;
 
     while(1)
     {
@@ -298,17 +209,11 @@ int main()
         // это условие выполняется, когда голова съедает яблоко
         if (appel.x == body.at(0).x  && appel.y == body.at(0).y  )
         {
-            //присваиваем новому телу координаты яблока
-            // body.push_back( appel );
-            //body.push_back(body.at(t));
-
             //закрашиваем съеденное яблоко
             cvFloodFill(field, appel, cvScalar(255, 255, 255));
 
-            // прибавить к туловищу змеи единицу
-            //body
             // рисуем яблоко
-            drawFood( field, cell, box_x, box_y, appel, r, g, b, ran);
+            drawFood( field, cell, box_x, box_y, appel, r, g, b, ran, body, number_of_bodies, number_of_apples);
             // считаем количество съеденных яблок
             number_of_apples++;
             // уменьшаем задержку(ускоряем змейку)
@@ -328,11 +233,11 @@ int main()
             direction = direc;
 
         // перемещение змейки
-        drawSnake( body, currFrame, number_of_apples);
+        drawSnake( body, currFrame, number_of_apples, number_of_bodies, end, finish);
         moveSnake( direction, body, cell);
         cvCircle(currFrame, body.at(0), 25, CV_RGB( 250, 0, 0), -1, 8);
         // конец змеи
-        end_snake(direc, finish, number_of_apples, body, width_field, height_field, exit);
+        end_snake(direc, finish, number_of_apples, body, width_field, height_field, exit, number_of_bodies);
         if (exit == 1) break;
 
         cvShowImage( nwPlayField, currFrame );
